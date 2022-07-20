b594ac8f6095570dd4e005b9a0989a352136a983
Sahil <sahildalvi54321@gmail.com>
2022-07-20 11:09:13 +0530

    Updates

---
+++ precache-manifest.0a6f10bd623e80ff355023e9d1955bd0.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "bac0405b5139bf5639ca",
+    "url": "/TextForm/static/css/main.80dbcf31.chunk.css"
+  },
+  {
+    "revision": "bac0405b5139bf5639ca",
+    "url": "/TextForm/static/js/main.dc963d47.chunk.js"
+  },
+  {
+    "revision": "e101851332dfa0f95a29",
+    "url": "/TextForm/static/js/runtime~main.7eaf80ed.js"
+  },
+  {
+    "revision": "5b10c54f526573367a3d",
+    "url": "/TextForm/static/js/2.f63325ab.chunk.js"
+  },
+  {
+    "revision": "2ded26446bc94b81347a",
+    "url": "/TextForm/static/js/3.398cb210.chunk.js"
+  },
+  {
+    "revision": "92b7f6aa03a08cf63d4687b37f09e653",
+    "url": "/TextForm/index.html"
+  }
+];
